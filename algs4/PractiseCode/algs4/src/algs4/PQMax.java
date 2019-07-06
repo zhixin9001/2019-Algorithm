199c0e9719d0fe9a9a3b0a7f298cc375751ca14e
dz33 <dante.x.zhi@newegg.com>
2019-07-06 19:01:28 +0800

    maxpq

---
+++ algs4/PractiseCode/algs4/src/algs4/PQMax.java
@@ -0,0 +1,80 @@
+package algs4;
+
+import java.util.Comparator;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Stopwatch;
+
+public class PQMax<Key extends Comparable<Key>> {
+    private Key[] pq;
+    private int N = 0;
+
+    public PQMax() {
+        int maxN=25;
+        pq = (Key[]) new Comparable[maxN + 1];
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void insert(Key v) {
+        pq[++N] = v;
+        swim(N);
+    }
+
+    public Key delMax() {
+        Key max = pq[1];
+        exch(1, N--);
+        pq[N + 1] = null;
+        sink(1);
+        return max;
+    }
+
+    private void sink(int k) {
+        while (2 * k <= N) {
+            int j = 2 * k;
+            if (j < N && less(j, j + 1)) {
+                j++;
+            }
+            if (!less(k, j)) {
+                break;
+            }
+            exch(k, j);
+            k = j;
+        }
+    }
+
+    private void swim(int k) {
+        while (k > 1 && less(k / 2, k)) {
+            exch(k / 2, k);
+            k = k / 2;
+        }
+    }
+
+    private boolean less(int i, int j) {
+        return pq[i].compareTo(pq[j]) < 0;
+    }
+
+    private void exch(int i, int j) {
+        Key t = pq[i];
+        pq[i] = pq[j];
+        pq[j] = t;
+    }
+
+    public static void main(String[] args) {
+        PQMax<String> pq = new PQMax<String>();
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            if (!item.equals("-"))
+                pq.insert(item);
+            else if (!pq.isEmpty())
+                StdOut.print(pq.delMax() + " ");
+        }
+        StdOut.println("(" + pq.size() + " left on pq)");
+    }
+}
