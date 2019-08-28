ff78cbc369b3707f586eb36a2eef052b002b768d
dz33 <dante.x.zhi@newegg.com>
2019-08-28 12:37:30 +0800

    8.28

---
+++ algs4/PractiseCode/algs4/src/algs4/two/Merge.java
@@ -0,0 +1,84 @@
+package algs4.two;
+
+import java.util.Comparator;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Stopwatch;
+
+public class Merge {
+    private static Comparable[] aux;
+
+    private static void merge(Comparable[] a, int lo, int mid, int hi) {
+        int i = lo, j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = a[k];
+        }
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) {
+                a[k] = aux[j++]; // ?
+            } else if (j > hi) {
+                a[k] = aux[i++];
+            } else if (less(aux[i], aux[j])) {
+                a[k] = aux[i++];
+            } else {
+                a[k] = aux[j++];
+            }
+        }
+    }
+
+    public static void sort(Comparable[] a) {
+        aux=new Comparable[a.length];
+        sort(a, 0, a.length - 1);
+    }
+
+    public static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) {
+            return;
+        }
+        int mid = lo + (hi - lo) / 2;
+        sort(a, lo, mid);
+        sort(a, mid + 1, hi);
+        merge(a, lo, mid, hi);
+    }
+
+    private static boolean less(Comparable a, Comparable b) {
+        return a.compareTo(b) < 0;
+    }
+
+    // private static void exch(Comparable[] a, int i, int j) {
+    // Comparable swap = a[i];
+    // a[i] = a[j];
+    // a[j] = swap;
+    // }
+
+    private static void show(Comparable[] a) {
+        for (Comparable c : a) {
+            // StdOut.print(c + " ");
+            StdOut.println(c);
+        }
+        // StdOut.println();
+    }
+
+    private static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] input = StdIn.readAllInts();
+        Integer[] a1 = new Integer[input.length];
+        for (int i = 0; i < input.length; i++) {
+            a1[i] = input[i];
+        }
+
+        // Stopwatch timer = new Stopwatch();
+        sort(a1);
+        assert isSorted(a1);
+        show(a1);
+        // StdOut.println(timer.elapsedTime());
+    }
+}
