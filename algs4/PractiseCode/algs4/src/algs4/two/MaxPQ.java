e2ce5ff79cbe014056272a4576b50c8f94374491
zhixin9001 <zhixin9001@live.com>
2019-09-10 22:15:06 +0800

    9.10

---
+++ algs4/PractiseCode/algs4/src/algs4/two/MaxPQ.java
@@ -0,0 +1,45 @@
+package algs4.two;
+
+import java.util.Comparator;
+
+public class MaxPQ<Key extends Comparable<Key>> {
+    private Key[] pq;
+    private int N = 0;
+
+    public MaxPQ(int maxN) {
+        pq = (Key[]) new Comparable[maxN + 1];
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void insert(Key v) {
+        pq[++N] = v;
+        swim(N);
+    }
+
+    public key delMax() {
+        Key max = pq[1];
+        exch(1, N--);
+        pq[N + 1] = null;
+        sink(1);
+        return max;
+    }
+
+    private boolean less(int i, int j) {
+        return pq[i].compareTo(pq[j]) < 0;
+    }
+
+    private void exch(int i, int j) {
+        Key t = pq[i];
+        pq[i] = pq[j];
+        pq[j] = t;
+    }
+
+    private 
+}
