75f079dea075fd0227e57d82903b5d4f96005f45
zhixin9001 <zhixin9001@live.com>
2019-10-19 07:03:27 +0800

    10.19

---
+++ algs4/PractiseCode/algs4/src/algs4/three/BlackRedBST.java
@@ -0,0 +1,101 @@
+package algs4.three;
+
+import java.util.Comparator;
+import java.util.NoSuchElementException;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import javafx.scene.Node;
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.StdDraw;
+import java.awt.Font;
+
+public class BlackRedBST<Key extends Comparable<Key>, Value> {
+    private static final boolean RED = true;
+    private static final boolean BLACK = false;
+
+    private class Node {
+        Key key;
+        Value val;
+        Node left, right;
+        int N;
+        boolean color;
+
+        Node(Key key, Value val, int N, boolean color) {
+            this.key = key;
+            this.val = val;
+            this.N = N;
+            this.color = color;
+        }
+    }
+
+    private boolean isRed(Node x) {
+        if (x == null)
+            return false;
+        return x.color == RED;
+    }
+
+    public int size(Node x) {
+        if (x == null)
+            return 0;
+        else
+            return x.size;
+    }
+
+    private Node rotataLeft(Node h) {
+        Node x = h.right;
+        h.right = x.left;
+        x.left = h;
+        x.color = h.color;
+        h.color = RED;
+        x.N = h.N;
+        h.N = size(h.left) + size(h.right) + 1;
+        return x;
+    }
+
+    private Node rotateRight(Node h) {
+        Node x = h.left;
+        h.left = x.right;
+        x.right = h;
+        x.color = h.color;
+        h.color = RED;
+        x.N = h.N;
+        h.N = size(h.left) + size(h.right) + 1;
+        return x;
+    }
+
+    private void flipColors(Node h) {
+        h.color = RED;
+        h.left.color = BLACK;
+        h.right.color = BLACK;
+    }
+
+    public void put(Key key, Value val) {
+        root = put(root, key, val);
+        root.color = BLACK;
+    }
+
+    private Node put(Node h, Key key, Value val) {
+        if (h == null) {
+            return new Node(key, val, 1, RED);
+        }
+        int cmp = key.compareTo(h.key);
+        if (cmp < 0) {
+            h.left = put(h.left, key, val);
+        } else if (cmp > 0) {
+            h.right = put(h.right, key, val);
+        } else {
+            h.val = val;
+        }
+
+        if (isRed(h.right) && !isRed(h.left))
+            h = rotataLeft(h);
+        if (isRed(h.left) && isRed(h.left.left))
+            h = rotateRight(h);
+        if (isRed(h.left) && isRed(h.right))
+            flipColors(h);
+
+        h.N = size(h.left) + size(h.right) + 1;
+
+        return h;
+    }
+}
--- algs4/PractiseCode/algs4/src/algs4/three/SeparateChainingHashST.java
+++ algs4/PractiseCode/algs4/src/algs4/three/SeparateChainingHashST.java
@@ -1,5 +1,6 @@
 package algs4.three;
 
+import java.security.Key;
 import java.util.Comparator;
 import java.util.NoSuchElementException;
 import edu.princeton.cs.algs4.StdIn;
@@ -7,33 +8,109 @@ import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.StdDraw;
 import algs4.three.SequentialSearchST;
+// import java.util.Queue;
 
 public class SeparateChainingHashST<Key, Value> {
-    private int N;   //count of k-v pairs
-    private int M;   //size of hashtable
-    private SequentialSearchST<Key,Value>[] st;
+    private static final int INIT_CAPACITY = 4;
 
-    public SeparateChainingHashST(){
-        this(997);
+    private int n; // count of k-v pairs
+    private int m; // size of hashtable
+    private SequentialSearchST<Key, Value>[] st;
+
+    public SeparateChainingHashST() {
+        this(INIT_CAPACITY);
     }
 
-    public SeparateChainingHashST(int M){
-        this.M=M;
-        st= (SequentialSearchST<Key,Value>[])new SequentialSearchST[M];
-        for(int i=0;i<M;i++){
-            st[i]=new SequentialSearchST();
+    public SeparateChainingHashST(int M) {
+        this.m = M;
+        st = (SequentialSearchST<Key, Value>[]) new SequentialSearchST[M];
+        for (int i = 0; i < M; i++) {
+            st[i] = new SequentialSearchST();
         }
     }
 
-    private int hash(Key key){
-        return (key.hashCode()&0x7fffffff)%M;
+    private int hash(Key key) {
+        return (key.hashCode() & 0x7fffffff) % m;
     }
 
-    public Value get(Key key){
-        return (Value)st[hash(key)].get(key);
+    public Value get(Key key) {
+        if (key == null)
+            throw new IllegalArgumentException("argument to get() is null");
+        return (Value) st[hash(key)].get(key);
     }
 
-    public void put(Key key,Value val){
-        st[hash(key)].put(key,val);
+    public void put(Key key, Value val) {
+        if (key == null)
+            throw new IllegalArgumentException("first argument to put() is null");
+
+        if (val == null) {
+            delete(key);
+            return;
+        }
+        // double table size if average length of list >= 10
+        if (n >= 10 * m)
+            resize(2 * m);
+
+        int i = hash(key);
+        if (!st[i].contains(key))
+            n++;
+        st[i].put(key, val);
+    }
+
+    public void delete(Key key) {
+        if (key == null)
+            throw new IllegalArgumentException("argument to delete() is null");
+
+        int i = hash(key);
+        if (st[i].contains(key))
+            n--;
+        st[i].delete(key);
+
+        if (m < INIT_CAPACITY && size() < 2 * m)
+            resize(m / 2);
+    }
+
+    public void resize(int chains) {
+        SeparateChainingHashST<Key, Value> temp = new SeparateChainingHashST<Key, Value>(chains);
+        for (int i = 0; i < m; i++) {
+            for (Key key : st[i].keys()) {
+                temp.put(key, st[i].get(key));
+            }
+        }
+        this.m = temp.m;
+        this.n = temp.n;
+        this.st = temp.st;
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public boolean contains(Key key) {
+        if (key == null)
+            throw new IllegalArgumentException("argument to contains() is null");
+        return get(key) != null;
+    }
+
+    public Iterable<Key> keys() {
+        Queue<Key> queue = new Queue<Key>();
+        for (int i = 0; i < m; i++) {
+            for (Key key : st[i].keys())
+                queue.enqueue(key);
+        }
+        return queue;
+    }
+
+    public static void main(String[] args) {
+        SeparateChainingHashST<String, Integer> st = new SeparateChainingHashST<String, Integer>();
+        for (int i = 0; !StdIn.isEmpty(); i++) {
+            String key = StdIn.readString();
+            st.put(key, i);
+        }
+
+        StdOut.println();
+
+        for (String s : st.keys())
+            StdOut.println(s + "-" + st.get(s));
     }
 }
