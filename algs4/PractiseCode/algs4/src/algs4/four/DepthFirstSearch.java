cfbb812b8e4aabe6bfc77441cf3e42f0e4ccf4b4
zhixin9001 <zhixin9001@live.com>
2019-10-30 23:06:09 +0800

    10.30

--- algs4/PractiseCode/algs4/src/algs4/four/DepthFirstSearch.java
+++ algs4/PractiseCode/algs4/src/algs4/four/DepthFirstSearch.java
@@ -1,6 +1,8 @@
 package algs4.four;
 
 import java.util.NoSuchElementException;
+import java.util.Stack;
+
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.In;
@@ -8,9 +10,13 @@ import edu.princeton.cs.algs4.In;
 public class DepthFirstSearch {
     private boolean[] marked;
     private int count;
+    private int[] edgeTo;
+    private final int s;
 
     public DepthFirstSearch(Graph G, int s) {
         marked = new boolean[G.V()];
+        edgeTo = new int[G.V()];
+        this.s = s;
         dfs(G, s);
     }
 
@@ -18,8 +24,10 @@ public class DepthFirstSearch {
         marked[v] = true;
         count++;
         for (int w : G.adj(v)) {
-            if (!marked[w])
+            if (!marked[w]) {
+                edgeTo[w] = v;
                 dfs(G, w);
+            }
         }
     }
 
@@ -31,20 +39,51 @@ public class DepthFirstSearch {
         return count;
     }
 
+    public boolean hasPathTo(int v) {
+        return marked(v);
+    }
+
+    public Iterable<Integer> pathTo(int v) {
+        if (!hasPathTo(v))
+            return null;
+
+        Stack<Integer> path = new Stack<Integer>();
+        for (int x = v; x != s; x = edgeTo[x]) {
+            path.push(x);
+        }
+
+        path.push(s);
+        return path;
+    }
+
     public static void main(String[] args) {
         In in = new In(args[0]);
         Graph G = new Graph(in);
         int s = Integer.parseInt(args[1]);
         DepthFirstSearch search = new DepthFirstSearch(G, s);
-        for (int v = 0; v < G.V(); v++) {
-            if (search.marked(v))
-                StdOut.print(v + " ");
-        }
 
-        StdOut.println();
-        if (search.count() != G.V())
-            StdOut.println("NOT connected");
-        else
-            StdOut.println("connected");
+        //
+        // for (int v = 0; v < G.V(); v++) {
+        // if (search.marked(v))
+        // StdOut.print(v + " ");
+        // }
+
+        // StdOut.println();
+        // if (search.count() != G.V())
+        // StdOut.println("NOT connected");
+        // else
+        // StdOut.println("connected");
+
+        //
+        for (int v = 0; v < G.V(); v++) {
+            StdOut.print(s+" to "+v+": ");
+            if(search.hasPathTo(v)){
+                for(int x:search.pathTo(v)){
+                    if(x==s) StdOut.print(x);
+                    else StdOut.print("-"+x);
+                }
+            }
+            StdOut.println();
+        }
     }
 }
