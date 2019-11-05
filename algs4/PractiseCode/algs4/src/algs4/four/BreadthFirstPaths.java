8b54114eb705f44f3170fd5f9d2ac7f419d9bad5
dz33 <dante.x.zhi@newegg.com>
2019-11-05 12:28:00 +0800

    11.5

---
+++ algs4/PractiseCode/algs4/src/algs4/four/BreadthFirstPaths.java
@@ -0,0 +1,56 @@
+package algs4.four;
+
+import java.util.NoSuchElementException;
+// import java.util.Stack;
+
+import algs4.Queue;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Stack;
+
+public class BreadthFirstPaths {
+    private boolean[] marked;
+    private int[] edgeTo;
+    private final int s;
+
+    public BreadthFirstPaths(Graph G, int s) {
+        marked = new boolean[G.V()];
+        edgeTo = new int[G.V()];
+        this.s = s;
+        bfs(G, s);
+    }
+
+    private void bfs(Graph G, int s) {
+        Queue<Integer> queue = new Queue<Integer>();
+        marked[s] = true;
+        queue.enqueue(s);
+        while (!queue.isEmpty()) {
+            int v = queue.dequeue();
+            for (int w : G.adj(v)) {
+                if(!marked[w]){
+                    edgeTo[w]=v;
+                    marked[w]=true;
+                    queue.enqueue(w);
+                }
+            }
+        }
+    }
+
+    public boolean hasPathTo(int v){
+        return marked[v];
+    }
+
+    public Iterable<Integer> pathTo(int v) {
+        if (!hasPathTo(v))
+            return null;
+
+        Stack<Integer> path = new Stack<Integer>();
+        for (int a = v; a != s; a = edgeTo[a]) {
+            path.push(a);
+        }
+
+        path.push(s);
+        return path;
+    }
+}
