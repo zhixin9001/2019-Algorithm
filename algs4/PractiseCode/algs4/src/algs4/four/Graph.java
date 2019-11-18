a55cde413084a2723ec82bb8e7dc77f5a121e083
zhixin9001 <zhixin9001@live.com>
2019-11-19 07:01:23 +0800

    11.19

--- algs4/PractiseCode/algs4/src/algs4/four/Graph.java
+++ algs4/PractiseCode/algs4/src/algs4/four/Graph.java
@@ -6,7 +6,6 @@ import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.In;
 
 public class Graph {
-    private static final String NEWLINE = System.getProperty("line.separator");
     private final int V; // vertex
     private int E; // edge
     private Bag<Integer>[] adj;
@@ -46,25 +45,5 @@ public class Graph {
 
     public Iterable<Integer> adj(int v) {
         return adj[v];
-    }
-
-    public String toString() {
-        StringBuilder s = new StringBuilder();
-        s.append(V + " vertices, " + E + " edges " + NEWLINE);
-        for (int v = 0; v < V; v++) {
-            s.append(v + ": ");
-            for (int w : adj[v]) {
-                s.append(w + " ");
-            }
-            s.append(NEWLINE);
-        }
-        return s.toString();
-    }
-
-    // java algs4.four.Graph ..\..\..\algs4-data\tinyG.txt
-    public static void main(String[] args) {
-        In in = new In(args[0]);
-        Graph G = new Graph(in);
-        StdOut.println(G);
     }
 }
