2a008a59c98b714843897df8a2e106885e893172
zhixin9001 <zhixin9001@live.com>
2019-11-21 21:04:54 +0800

    11.21

---
+++ algs4/PractiseCode/algs4/src/algs4/four/DirectedCycle.java
@@ -0,0 +1,69 @@
+package algs4.four;
+
+import java.util.NoSuchElementException;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Stack;
+
+public class DirectedCycle {
+    private boolean[] marked;
+    private int[] edgeTo;
+    private Stack<Integer> cycle;
+    private boolean[] onStack;
+
+    public DirectedCycle(Digraph g) {
+        marked = new boolean[g.V()];
+        onStack = new boolean[g.V()];
+        edgeTo = new int[g.V()];
+        for (int v = 0; v < g.V(); v++) {
+            if (!marked[v]) {
+                dfs(g, v);
+            }
+        }
+    }
+
+    private void dfs(Digraph g, int v) {
+        marked[v] = true;
+        onStack[v] = true;
+
+        for (int w : g.adj(v)) {
+            if (this.hasCycle())
+                return;
+            else if (!marked[w]) {
+                edgeTo[w] = v;
+                dfs(g, w);
+            } else if (onStack[w]) {
+                cycle = new Stack<Integer>();
+                for (int x = v; x != w; x = edgeTo[x]) {
+                    cycle.push(x);
+                }
+
+                cycle.push(w);
+                cycle.push(v);
+            }
+        }
+
+        onStack[v] = false;
+    }
+
+    public boolean hasCycle() {
+        return cycle != null;
+    }
+
+    public Iterable<Integer> cycle() {
+        return cycle;
+    }
+
+    // cmd /c --% java algs4.four.DirectedCycle ..\..\..\algs4-data\tinyDG.txt 1 2 6
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        Digraph g = new Digraph(in);
+
+        DirectedCycle directedCycle = new DirectedCycle(g);
+        for (int v : directedCycle.cycle()) {
+            StdOut.print(v + " ");
+        }
+        StdOut.println();
+    }
+}
