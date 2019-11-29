04a80f13a0fb94a25508946afc6154dfa37d5f57
dz33 <dante.x.zhi@newegg.com>
2019-11-29 12:39:41 +0800

    11.29

---
+++ algs4/PractiseCode/algs4/src/algs4/four/Edge.java
@@ -0,0 +1,49 @@
+package algs4.four;
+
+import java.util.NoSuchElementException;
+import java.lang.RuntimeException;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.In;
+
+public class Edge implements Comparable<Edge> {
+    private final int v; // vertex
+    private final int w; // vertex
+    private final double weight; // vertex
+
+    public Edge(int v, int w, double weight) {
+        this.v = v;
+        this.w = w;
+        this.weight = weight;
+    }
+
+    public double weight() {
+        return this.weight;
+    }
+
+    public int either() {
+        return this.v;
+    }
+
+    public int other(int vertex) {
+        if (v == vertex)
+            return w;
+        if (w == vertex)
+            return v;
+        else
+            throw new RuntimeException("Inconsistent edge");
+    }
+
+    public int compareTo(Edge that) {
+        if (this.weight() < that.weight())
+            return -1;
+        else if (this.weight() > that.weight())
+            return 1;
+        else
+            return 0;
+    }
+
+    public String toString() {
+        return String.format("%d-%d %.2f", v, w, weight);
+    }
+}
---
+++ algs4/PractiseCode/algs4/src/algs4/four/EdgeWeightedGraph.java
@@ -0,0 +1,74 @@
+package algs4.four;
+
+import java.util.NoSuchElementException;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Stack;
+
+public class EdgeWeightedGraph {
+    private static final String NEWLINE = System.getProperty("line.separator");
+    private final int V; // vertex
+    private int E; // edge
+    private Bag<Edge>[] adj;
+
+    public EdgeWeightedGraph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<Edge>[]) new Bag[V];
+        for (int v = 0; v < V; v++) {
+            adj[v] = new Bag<Edge>();
+        }
+    }
+
+    public EdgeWeightedGraph(In in) {
+        this(in.readInt());
+        int E = in.readInt();
+        for (int i = 0; i < E; i++) {
+            int v = in.readInt();
+            int w = in.readInt();
+            double weight = in.readDouble();
+            Edge e = new Edge(v, w, weight);
+            addEdge(e);
+        }
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    public void addEdge(Edge e) {
+        int v = e.either(), w = e.other(v);
+        adj[v].add(e);
+        adj[w].add(e);
+        E++;
+    }
+
+    public Iterable<Edge> adj(int v) {
+        return adj[v];
+    }
+
+    public String toString(){
+        StringBuilder s = new StringBuilder();
+        s.append(V + " vertices, " + E + " edges " + NEWLINE);
+        for (int v = 0; v < V; v++) {
+            s.append(v + ": ");
+            for (Edge w : adj[v]) {
+                s.append(w + " | ");
+            }
+            s.append(NEWLINE);
+        }
+        return s.toString();
+    }
+
+    // cmd /c --% java algs4.four.EdgeWeightedGraph ..\..\..\algs4-data\tinyEWG.txt
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        EdgeWeightedGraph ewg = new EdgeWeightedGraph(in);
+        StdOut.println(ewg);
+    }
+}
