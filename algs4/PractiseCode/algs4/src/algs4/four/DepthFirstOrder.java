6a593758cd7f1a31a4670ae707a5bdd035862aec
zhixin9001 <zhixin9001@live.com>
2019-11-29 20:14:53 +0800

    11.29

--- algs4/PractiseCode/algs4/src/algs4/four/DepthFirstOrder.java
+++ algs4/PractiseCode/algs4/src/algs4/four/DepthFirstOrder.java
@@ -13,7 +13,7 @@ public class DepthFirstOrder {
     private Queue<Integer> post;
     private Stack<Integer> reversePost;
 
-    public DepthFirstOrder(Graph G) {
+    public DepthFirstOrder(Digraph G) {
         pre=new Queue<Integer>();
         post=new Queue<Integer>();
         reversePost=new Stack<Integer>();
@@ -24,7 +24,7 @@ public class DepthFirstOrder {
         }
     }
 
-    private void dfs(Graph G, int v) {
+    private void dfs(Digraph G, int v) {
         pre.enqueue(v);
         marked[v] = true;
         for (int w : G.adj(v)) {
