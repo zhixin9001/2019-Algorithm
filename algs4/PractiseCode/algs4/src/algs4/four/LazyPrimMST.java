a653f5927b440b5668aa9dd970c6e0b289d51a06
dz33 <dante.x.zhi@newegg.com>
2019-11-30 12:38:17 +0800

    11.30

---
+++ algs4/PractiseCode/algs4/src/algs4/four/LazyPrimMST.java
@@ -0,0 +1,57 @@
+package algs4.four;
+
+import java.util.NoSuchElementException;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Queue;
+
+public class LazyPrimMST {
+    private boolean[] marked;
+    private Queue<Edge> mst;
+    private MinPQ<Edge> pq;
+
+    public LazyPrimMST(EdgeWeightedGraph G) {
+        pq = new MinPQ<Edge>();
+        marked = new boolean[G.V()];
+        mst = new Queue<Edge>();
+        visit(G, 0);
+        while (!pq.isEmpty()) {
+            Edge e = pq.delMin();
+            int v = e.either(), w = e.other(v);
+            if (marked[v] && marked[w])
+                continue;
+
+            mst.enqueue(e);
+            if (!marked[v])
+                visit(G, v);
+            if (!marked[w])
+                visit(G, w);
+        }
+    }
+
+    public void visit(EdgeWeightedGraph G, int v) {
+        marked[v] = true;        
+        for (Edge e : G.adj(v)){            
+            if (!marked[e.other(v)]){
+                pq.insert(e);
+            }
+        }
+    }
+
+    public Iterable<Edge> edges() {
+        return mst;
+    }
+
+    // cmd /c --% java algs4.four.LazyPrimMST ..\..\..\algs4-data\tinyEWG.txt
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        EdgeWeightedGraph ewg = new EdgeWeightedGraph(in);
+        LazyPrimMST lazyPrim=new LazyPrimMST(ewg);
+        // StdOut.println(ewg);
+        for(Edge e:lazyPrim.edges()){
+            StdOut.println(e);
+        }        
+    }
+}
