be1f95dcf8aeb5a1fb2211aa29339578e61f7d73
dz33 <dante.x.zhi@newegg.com>
2019-12-24 12:40:25 +0800

    12.24

--- algs4/PractiseCode/algs4/src/algs4/four/DijkstraSP.java
+++ algs4/PractiseCode/algs4/src/algs4/four/DijkstraSP.java
@@ -6,53 +6,83 @@ import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.IndexMinPQ;
+import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.algs4.Queue;
 
 public class DijkstraSP {
     private DirectedEdge[] edgeTo;
     private double[] distTo;
     private IndexMinPQ<Double> pq;
+    int s;
 
     public DijkstraSP(EdgeWeightedDigraph G, int s) {
         edgeTo = new DirectedEdge[G.V()];
         distTo = new double[G.V()];
-        pq=new IndexMinPQ<Double>(G.V());
-        
-        for(int v=0;v<G.V();v++){
-            distTo[v]=Double.POSITIVE_INFINITY;
+        pq = new IndexMinPQ<Double>(G.V());
+        this.s = s;
+
+        for (int v = 0; v < G.V(); v++) {
+            distTo[v] = Double.POSITIVE_INFINITY;
         }
 
-        distTo[s]=0.0;
-        pq.insert(s,0.0);
-        while(!pq.isEmpty()){
-            relax(G,pq.delMin());
+        distTo[s] = 0.0;
+        pq.insert(s, 0.0);
+        while (!pq.isEmpty()) {
+            relax(G, pq.delMin());
         }
     }
 
-    public void visit(EdgeWeightedGraph G, int v) {
-        marked[v] = true;
-        for (Edge e : G.adj(v)) {
-            if (!marked[e.other(v)]) {
-                pq.insert(e);
+    private void relax(EdgeWeightedDigraph G, int v) {
+        for (DirectedEdge e : G.adj(v)) {
+            int w = e.to();
+            if (distTo[w] > distTo[v] + e.weight()) {
+                distTo[w] = distTo[v] + e.weight();
+                edgeTo[w] = e;
+                if (pq.contains(w))
+                    pq.change(w, distTo[w]);
+                else
+                    pq.insert(w, distTo[w]);
             }
         }
     }
 
-    public Iterable<Edge> edges() {
-        return mst;
+    public double distTo(int w) {
+        return distTo(w);
+    }
+
+    public boolean hasPathTo(int w) {
+        return distTo(w) != Double.POSITIVE_INFINITY;
+    }
+
+    public Iterable<Integer> pathTo(int v) {
+        if (!hasPathTo(v))
+            return null;
+
+        Stack<Integer> path = new Stack<Integer>();
+        for (int a = v; a != s; a = edgeTo[a]) {
+            path.push(a);
+        }
+
+        path.push(s);
+        return path;
     }
 
     // cmd /c --% java algs4.four.DijkstraSP ..\..\..\algs4-data\tinyEWG.txt
     public static void main(String[] args) {
         In in = new In(args[0]);
-        EdgeWeightedGraph ewg = new EdgeWeightedGraph(in);
-        DijkstraSP lazyPrim = new DijkstraSP(ewg);
-        // StdOut.println(ewg);
-        double weight = 0;
-        for (Edge e : lazyPrim.edges()) {
-            weight += e.weight();
-            StdOut.println(e);
+        EdgeWeightedDigraph ewdg = new EdgeWeightedDigraph(in);
+        int s = Integer.parseInt(args[1]);
+        DijkstraSP dijkstraSP = new DijkstraSP(ewdg, s);
+
+        for (int t = 0; t < ewdg.V(); t++) {
+            StdOut.println(s + " to " + t);
+            StdOut.printf(" (%4.2f): ", dijkstraSP.distTo(t));
+            if (dijkstraSP.hasPathTo(t)) {
+                for (DirectedEdge e : dijkstraSP.pathTo(t)) {
+                    StdOut.print(e + " ");
+                }
+            }
+            StdOut.println();
         }
-        StdOut.println(weight);
     }
 }
