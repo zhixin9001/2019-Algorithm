d0a83db9400cdea2af02092bc1a5267ab9bdb531
dz33 <dante.x.zhi@newegg.com>
2020-01-02 12:42:05 +0800

    1.2

--- algs4/PractiseCode/algs4/src/algs4/five/TrieST.java
+++ algs4/PractiseCode/algs4/src/algs4/five/TrieST.java
@@ -2,10 +2,12 @@ package algs4.five;
 
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Queue;
 
 public class TrieST<Value> {
     private static int R = 256;
     private Node root;
+    private int n;
 
     private static class Node {
         private Object val;
@@ -34,8 +36,10 @@ public class TrieST<Value> {
 
     public Node put(Node x, String key, Value val, int d) {
         if (x == null)
-            return new Node();
+            x = new Node();
         if (d == key.length()) {
+            if (x.val == null)
+                n++;
             x.val = val;
             return x;
         }
@@ -45,16 +49,97 @@ public class TrieST<Value> {
         return x;
     }
 
+    public int size() {
+        return n;
+    }
+
+    public Iterable<String> keys() {
+        return keysWithPrefix("");
+    }
+
+    public Iterable<String> keysWithPrefix(String pre) {
+        Queue<String> q = new Queue<String>();
+        collect(get(root, pre, 0), pre, q);
+        return q;
+    }
+
+    private void collect(Node x, String pre, Queue<String> q) {
+        if (x == null)
+            return;
+        if (x.val != null)
+            q.enqueue(pre);
+        for (char c = 0; c < R; c++) {
+            collect(x.next[c], pre + c, q);
+        }
+    }
+
+    public Iterable<String> keysThatMatch(String pat) {
+        Queue<String> q = new Queue<String>();
+        collect(root, "", pat, q);
+        return q;
+    }
+
+    private void collect(Node x, String pre, String pat, Queue<String> q) {
+        int d = pre.length();
+        if (x == null)
+            return;
+        if (d == pat.length() && x.val != null)
+            q.enqueue(pre);
+        if (d == pat.length())
+            return;
+        char next = pat.charAt(d);
+        for (char c = 0; c < R; c++) {
+            if (next == '.' || next == c)
+                collect(x.next[c], pre + c, pat, q);
+        }
+    }
+
+    public String longestPrefixOf(String s) {
+        int length = search(root, s, 0, 0);
+        return s.substring(0, length);
+    }
+
+    public int search(Node x, String s, int d, int length) {
+        if (x == null)
+            return length;
+        if (x.val != null)
+            length = d;
+        if (d == s.length())
+            return length;
+        char c = s.charAt(d);
+        return search(x.next[c], s, d + 1, length);
+    }
+
     // cmd /c --% java algs4.five.TrieST < ..\..\..\algs4-data\shellsST.txt
     public static void main(String[] args) {
         TrieST<Integer> st = new TrieST<Integer>();
         for (int i = 0; !StdIn.isEmpty(); i++) {
             String key = StdIn.readString();
-            StdOut.println(key);
             st.put(key, i);
         }
 
-        StdOut.println(st.get("by"));
+        StdOut.println("keys(\"\"):");
+        for (String key : st.keys()) {
+            StdOut.println(key + " " + st.get(key));
+        }
+        StdOut.println();
 
+        
+        StdOut.println("longestPrefixOf(\"shellsort\"):");
+        StdOut.println(st.longestPrefixOf("shellsort"));
+        StdOut.println();
+
+        StdOut.println("longestPrefixOf(\"quicksort\"):");
+        StdOut.println(st.longestPrefixOf("quicksort"));
+        StdOut.println();
+
+        StdOut.println("keysWithPrefix(\"shor\"):");
+        for (String s : st.keysWithPrefix("shor"))
+            StdOut.println(s);
+        StdOut.println();
+
+        StdOut.println("keysThatMatch(\".he.l.\"):");
+        for (String s : st.keysThatMatch(".he.l."))
+            StdOut.println(s);
     }
 }
